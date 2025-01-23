37057bcf020fad2ba64153ed825ea4883a8ed1d0
Technados <117394920+Technados@users.noreply.github.com>
2025-01-23 08:09:14 -0500

    IDK What's going on

---
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+package frc.robot.subsystems;
+
+public class RollerSubsystem {
+    
+}
+=======
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RollerConstants;
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+/** Class to run the rollers over CAN */
+public class RollerSubsystem extends SubsystemBase {
+  private final SparkMax rollerMotor;
+
+  public RollerSubsystem() {
+    // Set up the roller motor as a brushed motor
+    rollerMotor = new SparkMax(RollerConstants.kRollerMotorCANID, MotorType.kBrusheless);
+
+    // Set can timeout. Because this project only sets parameters once on
+    // construction, the timeout can be long without blocking robot operation. Code
+    // which sets or gets parameters during operation may need a shorter timeout.
+    rollerMotor.setCANTimeout(250);
+
+    // Create and apply configuration for roller motor. Voltage compensation helps
+    // the roller behave the same as the battery
+    // voltage dips. The current limit helps prevent breaker trips or burning out
+    // the motor in the event the roller stalls.
+    SparkMaxConfig rollerConfig = new SparkMaxConfig();
+    rollerConfig.voltageCompensation(12);
+    rollerConfig.smartCurrentLimit(.3);
+    rollerMotor.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  // Command to run the roller with joystick inputs
+  public Command runRoller(
+      CANRollerSubsystem rollerSubsystem, DoubleSupplier forward, DoubleSupplier reverse) {
+    return Commands.run(
+        () -> rollerMotor.set(forward.getAsDouble() - reverse.getAsDouble()), rollerSubsystem);
+  }
+
+}
+>>>>>>> 45d0dd8c6a167a97de6206e34746a00057e00f08
